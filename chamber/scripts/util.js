c7a2619ed2bd1c73d68228fad42e24ee0b4c1fdd
great <jgreat770@gmail.com>
2026-02-02 15:50:33 +0100

    membership applicaation

--- chamber/scripts/util.js
+++ chamber/scripts/util.js
@@ -16,3 +16,17 @@ lastModified.textContent = document.lastModified;
 let yearSpan = document.getElementById("year");
 let currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
+
+document.getElementById("timestamp").value = new Date().toISOString();
+
+
+
+const params = new URLSearchParams(window.location.search);
+
+document.getElementById("name").textContent =
+  `${params.get("fname")} ${params.get("lname")}`;
+
+document.getElementById("email").textContent = params.get("email");
+document.getElementById("phone").textContent = params.get("phone");
+document.getElementById("business").textContent = params.get("business");
+document.getElementById("timestamp").textContent = params.get("timestamp");
